85452ae6aed88ba4372b327c9afcfc61c7a61031
Muhammad Rizky <62334810+MacroMachiinee@users.noreply.github.com>
2023-10-18 11:55:54 +0700

    Update about.py

--- Halaman/about.py
+++ Halaman/about.py
@@ -1,1 +1,14 @@
+"""This modules contains data about about page"""
 
+# Import necessary modules
+import streamlit as st
+
+def app():
+    """This function creates the about page"""
+    st.balloons()
+    st.title('Hubungi Saya')
+    st.markdown('''### Name:
+    Muhammad Rizky''')
+    st.markdown('''### Email:
+    sspdav02@gmail.com''')
+    st.markdown('''### GitHub: [MacroMachiinee](https://github.com/MacroMachiinee)'''
