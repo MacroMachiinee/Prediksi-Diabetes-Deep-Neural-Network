5b3ac39a96f9d69ebdad3612dcd7cd6f00c27a35
Muhammad Rizky <62334810+MacroMachiinee@users.noreply.github.com>
2023-10-18 13:27:03 +0700

    Update predict_diabetes_DNN.py

--- predict_diabetes_DNN.py
+++ predict_diabetes_DNN.py
@@ -14,7 +14,6 @@ st.title('Prediksi Diabetes Dengan Deep Neural Network')
 st.write("Diabetes adalah penyakit kronis yang ditandai dengan tingginya kadar gula darah. Gula darah adalah sumber energi utama bagi sel tubuh manusia. Akan tetapi, pada penderita diabetes, glukosa tersebut tidak dapat digunakan oleh tubuh.")
 st.write("Kadar gula darah dikendalikan oleh hormon insulin yang diproduksi pankreas. Namun, pada penderita diabetes, pankreas tidak mampu memproduksi insulin sesuai kebutuhan tubuh. Tanpa insulin, sel-sel tubuh tidak dapat menyerap dan mengolah glukosa menjadi energi.")
 st.write("Ada dua jenis diabetes, yaitu diabetes tipe 1 dan diabetes tipe 2. Diabetes tipe 1 adalah penyakit autoimun yang menyebabkan sistem kekebalan tubuh menyerang dan menghancurkan sel-sel penghasil insulin di pankreas. Diabetes tipe 2 adalah penyakit yang disebabkan oleh kombinasi faktor genetik, gaya hidup, dan obesitas.")
-st.write("Dataset ini berasal dari POSBINDU Puskesmas Mergangsan. Tujuan dari kumpulan data ini adalah untuk memprediksi secara diagnostik apakah pasien menderita diabetes atau tidak, berdasarkan pengukuran diagnostik tertentu yang disertakan dalam kumpulan data. Beberapa batasan ditempatkan pada pemilihan contoh-contoh ini dari basis data yang lebih besar.")
 st.write("-----------------------------------------------------------------------------------------------------------")
 
 st.title("Prediksi Diabetes")
